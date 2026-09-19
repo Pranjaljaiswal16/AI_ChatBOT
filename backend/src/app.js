a6cd975753ea64e41d08d5811a95d77e96a37f10
Pranjaljaiswal0416 <er.pranjaljaiswal@gmail.com>
2026-09-19 09:39:16 +0530

    serve frontend in production

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,22 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
 const projectRoot = path.resolve(__dirname, "../..");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
+// API routes yahan
+// app.use("/api/...", ...);
 
 // Frontend
-app.use(express.static(path.join(projectRoot, "frontend", "dist")));
+app.use(
+  express.static(path.join(projectRoot, "frontend", "dist"))
+);
 
-// React/Vite routes
-app.get(/^(?!\/api).*/, (req, res) => {
-  res.sendFile(path.resolve(projectRoot, "frontend", "dist", "index.html"));
+// React/Vite fallback — LAST route
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(
+    path.join(projectRoot, "frontend", "dist", "index.html")
+  );
 });
 
 module.exports = app;
